b06e4181c356979f5d0dacd74fc63ea3164d1ca6
Amangtt <amangtebeje123@gmail.com>
2025-02-25 15:55:08 +0300

    header

---
+++ dashboard/frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <header className="bg-dark text-white p-3">
+            <h1>Brent Oil Price Analysis and Prediction Dashboard</h1>
+          
+        </header>
+    );
+}
+
+export default Header;
